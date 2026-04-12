170f0582017d8b1525c70fcf927140cf9d70956b
Daniel Rodrigues Ribeiro <daniyyelrodrigues@gmail.com>
2026-04-12 13:32:21 -0300

    feat: implement LayoutWrapper component for consistent layout structure

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,19 +1,18 @@
 import "./App.css";
 import { Container } from "./components/layouts/Container";
 import { Footer } from "./components/layouts/Footer";
+import { LayoutWrapper } from "./components/layouts/LayoutWrapper";
 import { Navbar } from "./components/layouts/Navbar";
 
 function App() {
   return (
-    <div
-      style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
-    >
+    <LayoutWrapper>
       <Navbar />
       <Container>
         <h1>Conteúdo Principal</h1>
       </Container>
       <Footer />
-    </div>
+    </LayoutWrapper>
   );
 }
 
