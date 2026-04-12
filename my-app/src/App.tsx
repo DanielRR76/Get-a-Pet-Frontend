c985a4569c759a2d23a301d31a1a52142acdb3c0
Daniel Rodrigues Ribeiro <daniyyelrodrigues@gmail.com>
2026-04-12 13:41:45 -0300

    feat: replace main content placeholder with Home component in App

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,13 +3,14 @@ import { Container } from "./components/layouts/Container";
 import { Footer } from "./components/layouts/Footer";
 import { LayoutWrapper } from "./components/layouts/LayoutWrapper";
 import { Navbar } from "./components/layouts/Navbar";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
     <LayoutWrapper>
       <Navbar />
       <Container>
-        <h1>Conteúdo Principal</h1>
+        <Home />
       </Container>
       <Footer />
     </LayoutWrapper>
