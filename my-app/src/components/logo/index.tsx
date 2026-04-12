18dcef151844d8df0331235a242008e3bb4f6812
Daniel Rodrigues Ribeiro <daniyyelrodrigues@gmail.com>
2026-04-12 13:40:28 -0300

    fix: correct import paths and adjust padding in Logo component styles

--- my-app/src/components/logo/index.tsx
+++ my-app/src/components/logo/index.tsx
@@ -1,14 +1,17 @@
-import { Button } from "../button";
-import { Icon } from "../icon/Icon";
-import { Typography } from "../typography";
+import { Button } from "../Button";
+import { Icon } from "../Icon/Icon";
+import { Typography } from "../Typography";
 import styles from "./styles.module.css";
 export function Logo() {
   return (
     <div className={styles.logo}>
       <Button
+        // size="large"
         icon={<Icon name="get-a-pet" size="huge" color="secondary" />}
         color="transparent"
         text={<Typography text="Get a Pet" variant="h2" size="large" />}
+        border="none"
+        radius="small"
       />
     </div>
   );
