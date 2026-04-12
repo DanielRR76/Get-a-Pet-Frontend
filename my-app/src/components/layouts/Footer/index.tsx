f170522d1f0d84464b4c41a3681f86a7556088bb
Daniel Rodrigues Ribeiro <daniyyelrodrigues@gmail.com>
2026-04-12 13:33:52 -0300

    fix: correct Typography import and add size prop to Footer component

--- my-app/src/components/layouts/Footer/index.tsx
+++ my-app/src/components/layouts/Footer/index.tsx
@@ -1,9 +1,9 @@
 import styles from "./styles.module.css";
-import { Typography } from "../../typography";
+import { Typography } from "../../Typography";
 export function Footer() {
   return (
     <footer className={styles.footer}>
-      <Typography text="Get a Pet &copy; 2026" variant="h5" />
+      <Typography size="medium" text="Get a Pet &copy; 2026" variant="h5" />
     </footer>
   );
 }
