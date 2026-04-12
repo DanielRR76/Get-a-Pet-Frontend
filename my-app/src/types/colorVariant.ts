c0379d61c6679e6702d84acabad1364a2841abe1
Daniel Rodrigues Ribeiro <daniyyelrodrigues@gmail.com>
2026-04-12 13:43:34 -0300

    feat: implement theme toggle functionality and update styles

--- my-app/src/types/colorVariant.ts
+++ my-app/src/types/colorVariant.ts
@@ -1,6 +1,5 @@
 export type ColorVariant =
   | "primary"
-  | "primary-dark"
   | "secondary"
   | "success"
   | "success-dark"
@@ -9,6 +8,4 @@ export type ColorVariant =
   | "info"
   | "disabled"
   | "transparent"
-  | "text"
-  | "text-black"
-  | "text-white";
+  | "text";
