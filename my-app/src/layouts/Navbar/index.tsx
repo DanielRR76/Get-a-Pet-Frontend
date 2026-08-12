cb878cb242f54510e7796a2767135d1629fe8bc4
Daniel Rodrigues Ribeiro <daniyyelrodrigues@gmail.com>
2026-08-12 16:57:44 -0300

    refactor(layouts): move Navbar and MenuOptions to layouts

--- my-app/src/layouts/Navbar/index.tsx
+++ my-app/src/layouts/Navbar/index.tsx
@@ -1,9 +1,9 @@
-import { Logo } from "../../Logo";
+import { Logo } from "../../components/Logo";
 import { MenuOptions } from "../MenuOptions";
 import styles from "./styles.module.css";
 export function Navbar() {
   return (
-    <nav className={styles.navbar}>
+    <nav className={`flex_align_center ${styles.navbar}`}>
       <Logo />
       <MenuOptions />
     </nav>
