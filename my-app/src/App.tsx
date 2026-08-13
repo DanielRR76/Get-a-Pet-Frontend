747aebd80612a83f2c22b3287d1692c926bc1bba
Daniel Rodrigues Ribeiro <daniyyelrodrigues@gmail.com>
2026-08-13 13:23:10 -0300

    feat: implement router with react-router

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,13 +1,17 @@
 import { LayoutWrapper, Navbar, Container, Footer } from "./layouts";
+import { Router, Routes } from "./router";
 
 function App() {
   return (
+    <Router>
       <LayoutWrapper>
         <Navbar />
         <Container>
+          <Routes />
         </Container>
         <Footer />
       </LayoutWrapper>
+    </Router>
   );
 }
 
