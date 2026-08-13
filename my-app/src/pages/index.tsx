4b4e0e8839a2451caa8bd1791b6cc739346ccd0c
Daniel Rodrigues Ribeiro <daniyyelrodrigues@gmail.com>
2026-08-13 13:20:28 -0300

    feat: implement MyPets page

--- my-app/src/pages/index.tsx
+++ my-app/src/pages/index.tsx
@@ -1,6 +1,7 @@
 export { Login } from "./Auth/Login";
 export { Register } from "./Auth/Register";
 export { Home } from "./Home";
+export { MyPets } from "./Pet/MyPets";
 export { MyAdoptions } from "./Pet/MyAdoptions";
 export { AddPet } from "./Pet/AddPet";
 export { Profile } from "./User/Profile";
