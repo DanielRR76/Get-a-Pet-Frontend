4cf932ce1035a01fc9645e4d25b5c1afa0ece1ac
Daniel Rodrigues Ribeiro <daniyyelrodrigues@gmail.com>
2026-08-13 13:05:58 -0300

    refactor: move layout components from components to layouts folder

--- my-app/src/layouts/index.tsx
+++ my-app/src/layouts/index.tsx
@@ -1,2 +1,7 @@
+export * from "./Container";
+export * from "./Footer";
+export * from "./LayoutWrapper";
+export * from "./Navbar";
+export * from "./MenuOptions";
 export * from "./PageSection";
 export * from "./Navbar";
