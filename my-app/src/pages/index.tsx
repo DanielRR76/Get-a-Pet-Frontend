ba8491df8097225cc4d72d8b94d3cda86805d9a9
Daniel Rodrigues Ribeiro <daniyyelrodrigues@gmail.com>
2026-08-13 13:08:03 -0300

    feat: implement EditPet page

--- my-app/src/pages/index.tsx
+++ my-app/src/pages/index.tsx
@@ -1,2 +1,6 @@
 export { Login } from "./Auth/Login";
 export { Register } from "./Auth/Register";
+export { Home } from "./Home";
+export { AddPet } from "./Pet/AddPet";
+export { Profile } from "./User/Profile";
+export { EditPet } from "./Pet/EditPet";
